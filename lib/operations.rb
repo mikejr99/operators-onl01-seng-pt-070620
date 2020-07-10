89c101718ec0ebc0ca01dfca85e3c1225e65eac2
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-10 11:15:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,12 @@
-require 'pry'
-binding.pry
+
 def unsafe?(speed)
- if speed <60 || > 40
-   return true
- else 
-   return false
+
 end
 
 
 
 def not_safe?(speed)
-	speed <60 || >40 ? return true : return false
+
 end
 	
 
